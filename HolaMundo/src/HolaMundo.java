23b85764afe0427d104424375d03ca87513d95b8
Fátima Vázquez <mfatima.vazquez@gmail.com>
2022-02-02 11:40:37 +0100

    Cambio desde consola

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		print ln "Hola mundo";
+		print ln "Cambio en consola";
 
 	}
 
